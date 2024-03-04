e137dbc44cf5aaf44e09841923fe40f90a659289
Pedro Maroli <80985683+psksjsidkdnc@users.noreply.github.com>
2024-03-04 01:11:59 +0000

    teste

---
+++ testte.cs
@@ -0,0 +1,15 @@
+
+public class teste{
+     int   numero;
+     int numero2;
+     int Res;
+
+     void start(){
+            Res =numero+numero2;
+            if(Res<=45){
+                
+            }
+
+     }
+
+}
